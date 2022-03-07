9c1e5be1abb39c162c16ee2b4cb7f85158cb63c7
DannyMyles <dannymyles1999@gmail.com>
2022-03-07 17:00:30 +0300

    This commit adds modified the base html

--- student/urls.py
+++ student/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('',views.students_form),
-    path('list/',views.students_list),
+    path('',views.students_form,name="student_insert"), #get and post request for insert operation
+    path('<int:id>/',views.students_form,name="student_update"), #get and post request for update operation
+    path('delete/<int:id>/',views.students_delete,name="student_delete"), #get and post request for delete operation
+    path('list/',views.students_list,name="student_list"), #get and post request for display all record operation
 
 ]
--- student/views.py
+++ student/views.py
@@ -7,15 +7,25 @@ def students_list(request):
     context = {'students_list':Student.objects.all()}
     return render(request, 'student/student_list.html',context)
 
-def students_form(request):
+def students_form(request, id=0):
     if request.method =='GET':
-        form = StudentForm()
+        if id==0:
+            form = StudentForm()
+        else :
+            student=Student.objects.get(pk=id)
+            form=StudentForm(instance=student)
         return render(request, 'student/student_form.html',{'form':form})
     else:
-        form = StudentForm(request.POST)
+        if id==0:
+         form = StudentForm(request.POST)
+        else:
+            student= Student.objects.get(pk=id)
+            form=StudentForm(instance=student)
         if form.is_valid():
             form.save()
-        redirect('/student/student_list')
+        redirect('/student/list')
 
-def students_delete(request):
-    return render(request, 'template_name')
+def students_delete(request,id):
+    student= Student.objects.get(pk=id)
+    student.delete()
+    redirect('/student/list')
