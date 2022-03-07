12ba2dd18e5d523a7b79857012f26053dcda5fa5
DannyMyles <dannymyles1999@gmail.com>
2022-03-07 12:04:14 +0300

    Added content tobase. html

--- student/views.py
+++ student/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def students_list(request):
-    return
+    return render(request, 'student/student_list.html')
 
 def students_form(request):
-    return
+    return render(request, 'student/student_form.html')
 
 def students_delete(request):
-    return
+    return render(request, 'template_name')
