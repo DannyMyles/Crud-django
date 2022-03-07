93287493d58eed85c4da84e4431f0d4ac30007b4
DannyMyles <dannymyles1999@gmail.com>
2022-03-07 11:09:08 +0300

    added models

--- student/models.py
+++ student/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+    stack=models.CharField(max_length=100)
+
+
+class Student(models.Model):
+    fullname=models.CharField(max_length=100)
+    students_code=models.CharField(max_length=4)
+    mobile=models.CharField(max_length=15)
+    stack=models.ForeignKey(Position, on_delete=models.CASCADE)
+
